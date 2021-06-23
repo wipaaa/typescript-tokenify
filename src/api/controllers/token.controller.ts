7a1080ffae02b7897411aa9a73378aa23531f1b4
wipaaa <ini.wipaaa@gmail.com>
2021-06-23 19:49:38 +0800

    Testing compiler

--- src/api/controllers/token.controller.ts
+++ src/api/controllers/token.controller.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from 'express';
 import { OK } from 'http-status';
+import { link } from '../../constants';
+import Compiler from '../../helpers/compiler.helper';
 
 export const index = (req: Request, res: Response) => {
   return res.status(OK).json({
@@ -8,8 +10,14 @@ export const index = (req: Request, res: Response) => {
 };
 
 export const create = (req: Request, res: Response) => {
+  const compiler = new Compiler();
+  const compiled = compiler
+    .from(link.stub)
+    .with(req.body.replacements)
+    .compile();
+
   return res.status(OK).json({
-    message: 'You are accessing the APIv1 create token end point!',
-    data: req.body,
+    message: 'New token created!',
+    data: compiled,
   });
 };
