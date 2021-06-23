686f0c5dbbbdd45e59580012c5e401360587d21d
wipaaa <ini.wipaaa@gmail.com>
2021-06-23 13:24:03 +0800

    Change to direct default exports object

--- src/constants/env.constant.ts
+++ src/constants/env.constant.ts
@@ -1,6 +1,8 @@
-const env = {
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export default {
   host: process.env.HOST || 'localhost',
   port: process.env.PORT || 5000,
 };
-
-export default env;
