44d9cf5b166d2f767a3763f7ea8f3ef7cd462908
wipaaa <ini.wipaaa@gmail.com>
2021-06-23 12:04:54 +0800

    Add constants handler

---
+++ src/constants/env.constant.ts
@@ -0,0 +1,6 @@
+const env = {
+  host: process.env.HOST || 'localhost',
+  port: process.env.PORT || 5000,
+};
+
+export default env;
---
+++ src/constants/index.ts
@@ -0,0 +1,3 @@
+import env from './env.constant';
+
+export { env };
